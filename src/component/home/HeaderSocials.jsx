4f49c0a7b1b286923ed7a25a3bb73849bdd3adb6
Tushar Gomkar <46417282+tushargomkar@users.noreply.github.com>
2023-08-17 12:30:59 +0530

    Update HeaderSocials.jsx
    added links

--- src/component/home/HeaderSocials.jsx
+++ src/component/home/HeaderSocials.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const HeaderSocials = () => {
   return (
      <div className="home_socials">
-        <a href="#link" className="home_social-link" target='_blank'>
+        <a href="https://github.com/tushargomkar" className="home_social-link" target='_blank'>
             <i className='fa-brands fa-instagram'></i>
         </a>
 
-        <a href="#link" className="home_social-link" target='_blank'>
+        <a href="https://github.com/tgomkar5" className="home_social-link" target='_blank'>
             <i className='fa-brands fa-twitter'></i>
         </a>
-        <a href="#link" className="home_social-link" target='_blank'>
+        <a href="https://github.com/tushargomkar" className="home_social-link" target='_blank'>
             <i className='fa-brands fa-behance'></i>
         </a>
-        <a href="#link" className="home_social-link" target='_blank'>
+        <a href="https://github.com/tushargomkar" className="home_social-link" target='_blank'>
             <i className='fa-brands fa-github'></i>
         </a>
-        <a href="#link" className="home_social-link" target='_blank'>
+        <a href="https://www.linkedin.com/in/tushargomkar/" className="home_social-link" target='_blank'>
            <i class='fa fa-linkedin'></i>
         </a>
      </div>
